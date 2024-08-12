7fd54cf5f459407cc230bf773ba557cc1d5d558b
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2024-08-12 22:23:18 +0700

    change bsdtar to tar for more compatibility

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Define variables
-THEME_URL="https://github.com/moktavizen/material-darker-jdownloader/archive/refs/heads/main.zip"
+THEME_URL="https://github.com/moktavizen/material-darker-jdownloader/archive/refs/heads/main.tar.gz"
 EXTRACT_DIR="material-darker-jdownloader-main"
 INSTALL_DIR="$HOME/.jd"
 FPAK_INSTALL_DIR="$HOME/.var/app/org.jdownloader.JDownloader/data/jdownloader"
 
 # Download and extract the zip file
 echo "Downloading and extracting theme..."
-curl -sL "$THEME_URL" | bsdtar -xf- --strip-components=1
+curl -L "$THEME_URL" | tar -xzf-
 
 # Function to create directories and copy files
 install_theme() {
